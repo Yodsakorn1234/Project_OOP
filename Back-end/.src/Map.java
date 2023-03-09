50a27f94ecd5008cc4ecd3fe899559f5d5d8ef9c
Yodsakorn <menming1234@outlook.com>
2023-03-09 10:01:50 +0700

    Map.java

---
+++ Back-end/.src/Map.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+public class Map {
+     protected  int Row ;
+     protected  int Colum ;
+     protected long[][] a = new long[Row][Colum];
+
+     public Map()
+     {
+           Row = Configurationfile.n;
+           Colum = Configurationfile.m;
+
+     }
+
+     public static void CreateRegion()
+     {
+
+     }
+     public static void RandomCityCenter()
+     {
+
+     }
+
+
+}
